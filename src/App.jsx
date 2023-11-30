e04d58bafc900e67eb42e23e1d8f5214ae4aa774
HOONKH <boospa0305@gmai.com>
2023-11-30 09:41:06 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="min-h-screen flex justify-center items-center ">
+      일어나
+      <img
+        className="w-[300px] animate__animated animate__wobble animate__infinite dura"
+        src="./images/tired.jpg"
+        alt="tired"
+      />
+      코딩해야지
+    </div>
+  );
+}
+
+export default App;
